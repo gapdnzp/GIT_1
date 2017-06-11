07b69481a086d1e2a8a6a569b18a2491b5e038ab
Горяинов Александр <gap@MacBook-Pro-Gorainov.local>
2017-06-11 11:06:58 +0300

    add MyVIewController

---
+++ GIT_1/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  GIT_1
+//
+//  Created by Горяинов Александр on 11.06.17.
+//  Copyright © 2017 GAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
